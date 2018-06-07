4100f43289cf7298a3a9b249ae13e0be84bce075
OlivierLeViet <orsn.tran@free.fr>
2018-06-07 18:20:13 +0200

    adlin
    rgge

--- scripts/reset_shows.sh
+++ scripts/reset_shows.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-#Lance les scripts pour la création de user, de la base et tables, et rempli les tables
-./create_admin_user.sh
+#Lance les scripts pour la création, de la base et tables, et rempli les tables
 ./create_show_database.sh
 ./load_show_data.sh
